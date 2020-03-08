730523dabc6388237f1e9a4ca8ea0d88b9519217
Shuaib Bashir <bashonbash@gmail.com>
2020-03-08 12:44:41 +0100

    Update payment gateway repository

--- src/main/java/com/queuepay/ibs/repositories/GatewayRepository.java
+++ src/main/java/com/queuepay/ibs/repositories/GatewayRepository.java
@@ -4,8 +4,7 @@ import com.queuepay.ibs.models.PaymentGateway;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-import java.util.UUID;
 
-public interface AuthRepository extends JpaRepository<PaymentGateway, UUID> {
+public interface GatewayRepository extends JpaRepository<PaymentGateway, Integer> {
     Optional<PaymentGateway> findByName(String name);
 }
