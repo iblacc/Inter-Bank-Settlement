497ce21881464d702573e28def21bd551c26f6fc
Shuaib Bashir <bashonbash@gmail.com>
2020-03-08 12:48:49 +0100

    update IbsApplication file

--- src/main/java/com/queuepay/ibs/IbsApplication.java
+++ src/main/java/com/queuepay/ibs/IbsApplication.java
@@ -1,9 +1,12 @@
 package com.queuepay.ibs;
 
-import org.modelmapper.ModelMapper;
+import com.queuepay.ibs.dto.Account;
+import com.queuepay.ibs.dto.CardValidation;
+import com.queuepay.ibs.dto.Token;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -13,8 +16,28 @@ public class IbsApplication {
         SpringApplication.run(IbsApplication.class, args);
     }
 
+//    @Bean
+//    public ModelMapper modelMapper() {
+//        return new ModelMapper();
+//    }
+
     @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public Token token() {
+        return new Token();
+    }
+
+    @Bean
+    public CardValidation cardValidation() {
+        return new CardValidation();
+    }
+
+    @Bean
+    public Account account() {
+        return new Account();
     }
 }
